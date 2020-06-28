c9b68253c6caab6300cdf49fa64af969dde023f4
code-senpai-coder <matyasbalcar0@gmail.com>
2020-06-28 21:32:39 +0200

    added sum func

--- main.py
+++ main.py
@@ -17,23 +17,49 @@ def findLastRow(tabulka):
         else:
             main = False
             return index
+
+def getSum(rowsNum, tabulka):
+    sumOfPrices = 0
+    for i in range(1, rowsNum):
+        sumOfPrices += float(tabulka["C" + str(i)].value)
+    return sumOfPrices
+
 def writeToLastRow(tabulka, lastrowindex, datum, jmeno, cena, workbook):
     tabulka["A" + str(lastrowindex)].value = datum
     tabulka["B" + str(lastrowindex)].value = jmeno
-    tabulka["C" + str(lastrowindex)].value = cena + "kc"
+    tabulka["C" + str(lastrowindex)].value = cena 
     workbook.save("test.xlsx")
-    dalsi = input("chcete zadat dalsi produkt? y/n")
+    dalsi = input("Pro pridani produktu zadej [y] a pro zobrazeni souctu cen napis [s]")
     if dalsi == "y":
+        writeToLastRow(sheet, findLastRow(sheet),input("Zadej datum: "),input("\nZadej jmeno produktu: "),input("\nZadej cenu produktu: "), wb )
+        print("Polozka uspesne pridana.")
+    elif dalsi == "s":
+        print(f"Součet všech položek je: {getSum((findLastRow(sheet)),sheet)} kc.")
+        nevimjakejmenolul = input("Pro pridani produktu zadej [y]")
+        if nevimjakejmenolul == "y":
+            writeToLastRow(sheet, findLastRow(sheet),input("Zadej datum: "),input("\nZadej jmeno produktu: "),input("\nZadej cenu produktu: "), wb )
+            print("Polozka uspesne pridana.")
+        else:
+            pass
+    else:
+        pass
+
+
+#print(getSum(findLastRow(sheet), sheet))
+#writeToLastRow(sheet, findLastRow(sheet),input("Zadej datum: "),input("\nZadej jmeno produktu: "),input("\nZadej cenu produktu: "), wb )
+base = input("Pro pridani produktu zadej [y] a pro zobrazeni souctu cen napis [s]")
+if base == "y":
+    print("Polozka uspesne pridana.")
+elif base == "s":
+    print(f"Součet všech položek je: {getSum((findLastRow(sheet)),sheet)}.")
+    nevimjakejmenolul2 = input("Pro pridani produktu zadej [y] ")
+    if nevimjakejmenolul2 == "y":
         writeToLastRow(sheet, findLastRow(sheet),input("Zadej datum: "),input("\nZadej jmeno produktu: "),input("\nZadej cenu produktu: "), wb )
         print("Polozka uspesne pridana.")
     else:
         pass
-writeToLastRow(sheet, findLastRow(sheet),input("Zadej datum: "),input("\nZadej jmeno produktu: "),input("\nZadej cenu produktu: "), wb )
+else:
+    pass
 
 
 
-
-
-
-
-    
